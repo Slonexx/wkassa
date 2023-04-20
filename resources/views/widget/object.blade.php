b1e83c2cb8f0bc89fcc2ddfe53e7be33da41dea2
Slonexxx <21moriarty21@mail.ru>
2023-04-20 14:27:44 +0600

    kassa new update

--- resources/views/widget/object.blade.php
+++ resources/views/widget/object.blade.php
@@ -7,7 +7,7 @@
                 <div class="mx-2 text-center"> <img src="https://smartwebkassa.kz/webkassa_png.png" width="50%"   alt=""> </div>
             </div>
             <div class="col-6 ">
-                <div class="mx-2 mt-3 text-right" style="font-size: 10px"> Важно! После фискализации закрывайте документ без сохранения </div>
+                <div class="mx-2 text-right" style="font-size: 10px"> Важно! После фискализации закрывайте документ без сохранения </div>
             </div>
         </div>
 
