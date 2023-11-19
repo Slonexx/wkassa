e7765643aa59d8561310314c5ff45041cbd14eba
Slonexxx <21moriarty21@mail.ru>
2023-11-20 00:27:45 +0600

    kassa new update

---
+++ app/Clients/testKassClient.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Clients;
+
+use App\Http\Controllers\BD\getMainSettingBD;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
+
+class testKassClient
+{
+    private Client $client;
+    private mixed $URL_WEBKASSA;
+    private getMainSettingBD $Setting;
+
+    public function __construct($accountId)
+    {
+        $this->URL_WEBKASSA = Config::get("Global");
+        $this->Setting = new getMainSettingBD($accountId);
+
+        $this->client = new Client([
+            'base_uri' => $this->URL_WEBKASSA['webkassa'].'api/',
+            'headers' => [
+                'x-api-key' => $this->URL_WEBKASSA['token_webkassa'],
+                'Content-Type' => 'application/json',
+            ]
+        ]);
+    }
+
+    public function testCheck($body): \Psr\Http\Message\ResponseInterface
+    {
+        return $this->client->post($this->URL_WEBKASSA['webkassa'].'api/Check',[
+            'body' => json_encode($body),
+        ]);
+    }
+
+
+    public function apiCashBoxes(): array
+    {
+        try {
+            $result = [];
+            $body = [ 'body' => json_encode(['Token'=> $this->Setting->authtoken]) ];
+            $tmp = json_decode($this->client->post($this->URL_WEBKASSA['webkassa'].'api/CashBoxes', $body)->getBody()->getContents());
+            if (property_exists($tmp, 'Data')) foreach ($tmp->Data->List as $item){ $result[] = $item; }
+            else { return []; }
+
+            if (property_exists($tmp, 'Errors')){
+                return [];
+            }
+            return $result;
+        } catch (BadResponseException $e){
+            return [];
+        }
+
+
+    }
+
+
+    public function MoneyOperation($CashboxUniqueNumber, $OperationType, $Sum){
+        $body = [
+            "token" => $this->Setting->authtoken,
+            "CashboxUniqueNumber" => $CashboxUniqueNumber,
+            "OperationType" => (int) $OperationType,
+            "Sum" =>(float) $Sum,
+            "ExternalCheckNumber" => Str::uuid()->toString(),
+        ];
+
+        //dd($body, json_encode($body));
+
+        $res = $this->client->post($this->URL_WEBKASSA['webkassa'].'api/MoneyOperation',[
+            'body' => json_encode($body),
+        ]);
+
+        return json_decode($res->getBody()->getContents());
+    }
+
+
+    public function XReport($CashboxUniqueNumber){
+        $body = [
+            "token" => $this->Setting->authtoken,
+            "CashboxUniqueNumber" => $CashboxUniqueNumber,
+        ];
+        $res = $this->client->post($this->URL_WEBKASSA['webkassa'].'api/XReport',[
+            'body' => json_encode($body),
+        ]);
+
+        return json_decode($res->getBody()->getContents());
+    }
+
+    public function ZReport($CashboxUniqueNumber){
+        $body = [
+            "token" => $this->Setting->authtoken,
+            "CashboxUniqueNumber" => $CashboxUniqueNumber,
+        ];
+        $res = $this->client->post($this->URL_WEBKASSA['webkassa'].'api/ZReport',[
+            'body' => json_encode($body),
+        ]);
+
+        return json_decode($res->getBody()->getContents());
+    }
+
+
+    public function postCheck($body){
+        $body["token"] = $this->Setting->authtoken;
+        $body["CashboxUniqueNumber"] = $this->Setting->CashboxUniqueNumber;
+
+        $res = $this->client->post($this->URL_WEBKASSA['webkassa'].'api/Check',[
+            'body' => json_encode($body),
+        ]);
+
+        return json_decode($res->getBody());
+    }
+
+
+    public function TicketPrint($ExternalCheckNumber){
+
+        $res = $this->client->post($this->URL_WEBKASSA['webkassa'].'api/Ticket/PrintFormat',[
+            'body' => json_encode(
+                [
+                    "token" => $this->Setting->authtoken,
+                    "CashboxUniqueNumber" => $this->Setting->CashboxUniqueNumber,
+                    "isDuplicate" => false,
+                    "paperKind" => 0,
+                    "ExternalCheckNumber" => $ExternalCheckNumber,
+                ]
+            ),
+        ]);
+
+        return json_decode($res->getBody()->getContents());
+
+    }
+
+
+    public function ShiftHistory($skip, $Take){
+        try {
+            $body = [
+                "token" => $this->Setting->authtoken,
+                "CashboxUniqueNumber" => $this->Setting->CashboxUniqueNumber,
+                "Skip" => $skip,
+                "Take" => $Take,
+            ];
+            $res = $this->client->post($this->URL_WEBKASSA['webkassa'].'api/Cashbox/ShiftHistory',[
+                'body' => json_encode($body),
+            ]);
+        } catch (BadResponseException $e){
+            dd($e->getResponse()->getBody()->getContents());
+        }
+
+        return json_decode($res->getBody()->getContents());
+    }
+
+}
--- config/Global.php
+++ config/Global.php
@@ -3,7 +3,9 @@ return [
     'url' => env('APP_URL'),
 
     'webkassa' => env('APP_URL_WEBKASSA'),
+    'dev_webkassa' => env('APP_URL_WEBKASSA'),
     'token_webkassa' => env('APP_TOKEN'),
+    'dev_token_webkassa' => env('APP_TOKEN'),
 
     'ms' => env('APP_URL_MyWarehouseEntity'),
 ];
