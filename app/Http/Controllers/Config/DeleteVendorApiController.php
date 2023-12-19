2e156f021caec7b1ab2e3b136a50c957de49acb2
Slonexxx <21moriarty21@mail.ru>
2023-12-19 14:54:38 +0600

    kassa new update

--- app/Http/Controllers/Config/DeleteVendorApiController.php
+++ app/Http/Controllers/Config/DeleteVendorApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Config;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Exception\BadResponseException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -10,8 +11,13 @@ class DeleteVendorApiController extends Controller
 {
     public function delete($accountId){
 
-        $path = public_path().'/Config/data/'.$accountId.'.json';
-        unlink($path);
+        try {
+            $path = public_path().'/Config/data/'.$accountId.'.json';
+            unlink($path);
+        } catch (BadResponseException){
+
+        }
+        return response(200);
 
     }
 }
